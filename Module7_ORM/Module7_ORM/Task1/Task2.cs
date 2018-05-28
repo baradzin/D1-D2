a7a225a73251abf9521c4b5655ba8a156dfd12e4
Aliaksei Baradzin <Aliaksei_Baradzin@epam.com>
2018-05-29 01:02:25 +0300

    Upd task1 in module7

---
+++ Module7_ORM/Module7_ORM/Task1/Task2.cs
@@ -0,0 +1,75 @@
+﻿using DataModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqToDB;
+using LinqToDB.SqlProvider;
+
+namespace Task1
+{
+    [TestFixture]
+    public class Task2
+    {
+        private NorthwindDB db;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            db = new NorthwindDB();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            db.Close();
+        }
+
+        [Test]
+        public void ProductListWithCategoryAndSuppliers()
+        {
+            foreach (var product in db.Products.LoadWith(p => p.Category).LoadWith(p => p.Supplier))
+            {
+                Console.WriteLine("Product Name - " + product.ProductName + ", Category - " + product.Category.CategoryName + ", Supplier - " + product.Supplier.CompanyName);
+            }
+        }
+
+        [Test]
+        public void EmployeeListWithRegions()
+        {
+            var targetList = db.EmployeeTerritories.Select(x => new { x.Employee.FirstName, x.Employee.LastName, x.Territory.Region.RegionDescription }).Distinct();
+            foreach(var empl in targetList)
+            {
+                Console.WriteLine(empl.FirstName + " " + empl.LastName + " " + empl.RegionDescription);
+            }
+        }
+
+        [Test]
+        public void EmployeeRegionStatistic()
+        {
+            var targetList = db.EmployeeTerritories.Select(x => new { x.EmployeeID, x.Territory.Region })
+                .Distinct()
+                .GroupBy(x => x.Region.RegionDescription);
+            foreach(var set in targetList)
+            {
+                Console.WriteLine(set.Key + " Count: " + set.Count());
+            }
+        }
+
+        [Test]
+        public void EmployeeWithSuppliersList()
+        {
+            var targetList = db.Orders
+                .Select(x => new { EmployeeName = x.Employee.FirstName + " " + x.Employee.LastName, x.Shipper.CompanyName })
+                .Distinct()
+                .GroupBy(x => x.EmployeeName);
+            foreach(var set in targetList)
+            {
+                foreach(var ship in set)
+                Console.WriteLine(set.Key + " " + ship.CompanyName);
+            }
+        }
+    }
+}
---
+++ Module7_ORM/Module7_ORM/Task1/Task3.cs
@@ -0,0 +1,61 @@
+﻿using DataModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqToDB;
+using LinqToDB.SqlProvider;
+
+namespace Task1
+{
+    [TestFixture]
+    public class Task3
+    {
+        private NorthwindDB db;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            db = new NorthwindDB();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            db.Close();
+        }
+
+        //	Добавить нового сотрудника, и указать ему список территорий,
+        //    за которые он несет ответственность.
+        [Test]
+        public void InsertEmployeeWithTerritories()
+        {
+            var emplId = Convert.ToInt32(db.InsertWithIdentity(new Employee { FirstName = "Aliaksei", LastName = "Baradzin" }));
+            var territoriesSet = db.Territories.Take(2).ToArray();
+            db.Insert(new EmployeeTerritory { EmployeeID = emplId, TerritoryID = territoriesSet[0].TerritoryID });
+            db.Insert(new EmployeeTerritory { EmployeeID = emplId, TerritoryID = territoriesSet[1].TerritoryID });
+        }
+
+        //	Перенести продукты из одной категории в другую
+        [Test]
+        public void ChangeCategoryForProducts()
+        {
+            var rand = new Random();
+            var categoryId = db.Categories.ToArray()[rand.Next(db.Categories.Count())].CategoryID;
+            var product = db.Products.First(x => x.CategoryID != categoryId);
+            product.CategoryID = categoryId;
+            db.Update(product);
+            Assert.AreEqual(db.Products.First(x => x.ProductID == product.ProductID).CategoryID, categoryId);
+        }
+
+        //•	Добавить список продуктов со своими поставщиками и категориями (массовое занесение),
+        //при этом если поставщик или категория с таким названием есть, то использовать их – иначе создать новые. 
+        [Test]
+        public void FullSaveGraph()
+        {
+
+        }
+    }
+}
