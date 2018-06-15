84f48e6b5403c885b6d3ea64286d38c12a9ac2d0
Aliaksei Baradzin <Aliaksei_Baradzin@epam.com>
2018-06-15 15:34:58 +0300

    parser done

---
+++ Module11_LoggingMonitoring/Task/Task/LogParser/LogParser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interop.MSUtil;
+
+namespace LogParser
+{
+    public class LogParser
+    {
+        public string Path { get; set; }
+
+        private COMTSVInputContextClass loggerContext;
+
+        private readonly LogQueryClass logQueryClass;
+
+        public ILogRecordset Error
+        {
+            get
+            {
+                return logQueryClass.Execute($@"SELECT Field1 AS Date, Field2 AS Level, Field3 AS Message FROM {Path} WHERE Field2='ERROR'", loggerContext);
+            }
+        }
+
+        public ILogRecordset LevelsCount
+        {
+            get
+            {
+                return logQueryClass.Execute($@"SELECT Field2 AS Level, COUNT(*) AS LevelsCount FROM {Path} GROUP BY Field2", loggerContext);
+            }
+        }
+
+        public LogParser(string path)
+        {
+            this.Path = path;
+            
+            this.loggerContext = new COMTSVInputContextClass
+            {
+                headerRow = false,
+                iSeparator = " | ",
+                nFields = 3
+            };
+            this.logQueryClass = new LogQueryClass();
+        }
+    }
+}
---
+++ Module11_LoggingMonitoring/Task/Task/LogParser/Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LogParser
+{
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void TestMethod()
+        {
+            var logGenerator = new LogParser(@"E:\Mentoring\D1-D2\Module11_LoggingMonitoring\Task\Task\MvcMusicStore\logs\2018-06-15.log");
+
+            var errors = logGenerator.Error;
+            var count = logGenerator.LevelsCount;
+
+            while (!count.atEnd())
+            {
+                var row = count.getRecord();
+                Console.WriteLine($"{row.getValue("Level")} , {row.getValue("LevelsCount")}");
+                count.moveNext();
+            }
+
+            while (!errors.atEnd())
+            {
+                var row = errors.getRecord();
+                Console.WriteLine($"{row.getValue("Date")} , {row.getValue("Level")}, {row.getValue("Message")}");
+                errors.moveNext();
+            }
+        }
+    }
+}
